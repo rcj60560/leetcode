956b7d96893e9ce10540b5e4079d46b3894f318b
rcj60560 <289759379@qq.com>
2021-06-07 21:14:56 +0800

    从尾到头打印链表

---
+++ src/leetcode/_剑指06从尾到头打印链表.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+public class _剑指06从尾到头打印链表 {
+    public static void main(String[] args) {
+        System.out.println("---<");
+//        ListNode listNode4 = new ListNode(4, null);
+//        ListNode listNode3 = new ListNode(3, listNode4);
+//        ListNode listNode2 = new ListNode(2, listNode3);
+        ListNode listNode1 = new ListNode(1, null);
+
+        int[] ints = reversePrint(listNode1);
+        System.out.println(ints);
+
+    }
+
+    private static int[] reversePrint(ListNode node) {
+        if (node == null) {
+            return new int[]{};
+        }
+        ListNode listNode = node;
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        int[] nodeInt = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            nodeInt[i] = listNode.val;
+            listNode = listNode.next;
+        }
+
+        return nodeInt;
+    }
+
+    public static class ListNode {
+        private int val;
+        private ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
